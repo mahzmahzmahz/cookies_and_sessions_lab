278e9f6ffa05c861561e886996eedad5f878ecc0
Alex Marz <alexhmarz@gmail.com>
2021-01-19 17:29:18 -0500

    I can't see anything I'm writing because the lab tests are all messed up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :products, only: [:index]
+
+  get '/products/add' to: "products#add", as: 'add'
+
 end
